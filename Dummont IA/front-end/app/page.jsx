9a0d0f7367cbc3896535598332177599ff11f7b5
Professor Dummont <professordummontai@gmail.com>
2025-05-31 11:27:23 -0300

    Alteração 20250531 11:39

---
+++ Dummont IA/front-end/app/page.jsx
@@ -0,0 +1,11 @@
+import Header from '../components/Header';
+import ChatBox from '../components/ChatBox';
+
+export default function HomePage() {
+  return (
+    <div>
+      <Header />
+      <ChatBox />
+    </div>
+  );
+}
